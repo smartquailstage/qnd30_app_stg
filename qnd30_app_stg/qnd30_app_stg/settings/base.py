5d15fc4495ba0dcbd836352728f8c379ebd87074
smartquailprod <smartquail.stage@gmail.com>
2024-03-26 06:36:12 -0500

    UPDATE

--- qnd30_app_stg/qnd30_app_stg/settings/base.py
+++ qnd30_app_stg/qnd30_app_stg/settings/base.py
@@ -65,7 +65,7 @@ INSTALLED_APPS = [
     #'django_comments',
     #Wagtail Inicials
     'core',
-    'wagtail',
+    #'wagtail',
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
     'wagtail.embeds',
@@ -77,10 +77,10 @@ INSTALLED_APPS = [
     'wagtail.search',
 
     'wagtail.admin',
-    #'wagtail.core',
+    'wagtail.core',
     'wagtail.contrib.settings',
     'wagtail.contrib.routable_page',
-    #'wagtail.contrib.modeladmin',
+    'wagtail.contrib.modeladmin',
     'wagalytics',
     'wagtailfontawesome',
     'wagtailgmaps',
